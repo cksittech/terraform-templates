5b91d382d679d6c72f76d90264c3536132e8bc9a
cksittech <zerg.cfa44.cksittech@gmail.com>
2025-12-19 11:48:00 +0000

    create or update at v6.27.0

--- AWS/tf-sample/v6/networkmanager/networkmanager_site_to_site_vpn_attachment.tf
+++ AWS/tf-sample/v6/networkmanager/networkmanager_site_to_site_vpn_attachment.tf
@@ -1,6 +1,7 @@
 resource "aws_networkmanager_site_to_site_vpn_attachment" "tf-sample-networkmanager-site-to-site-vpn-attachment" {
-  core_network_id    = ""
-  vpn_connection_arn = ""
+  core_network_id      = ""
+  routing_policy_label = ""
+  vpn_connection_arn   = ""
   
   tags = {}
 }
