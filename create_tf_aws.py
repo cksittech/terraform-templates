7fa3e421aac92dc91aca672e0ab1e8e28f3bf231
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-01-18 14:40:33 +0900

    update create_tf_aws.py

--- create_tf_aws.py
+++ create_tf_aws.py
@@ -22,8 +22,8 @@ try:
     for resource in RESOURCE_SCHEMAS.keys():
         OUTPUT_PATH = f"./output/{resource.split('_')[1]}"
         os.makedirs(OUTPUT_PATH, exist_ok=True)
-        with open(f"./{OUTPUT_PATH}/{"_".join(resource.split('_')[1:])}.tf", "w", encoding="utf-8") as f:
-            f.write(f"resource \"{resource}\" \"tf-sample-{"-".join(resource.split('_')[1:])}\" {{\n")
+        with open(f"./{OUTPUT_PATH}/{'_'.join(resource.split('_')[1:])}.tf", "w", encoding="utf-8") as f:
+            f.write(f"resource \"{resource}\" \"tf-sample-{'-'.join(resource.split('_')[1:])}\" {{\n")
             tags_exist = True if "tags" in list(RESOURCE_SCHEMAS[resource]["block"]["attributes"].keys()) else False
             attributes = [attribute for attribute in list(RESOURCE_SCHEMAS[resource]["block"]["attributes"].keys()) 
                         if not(attribute in ["arn", "tags_all", "tags", "id"]) 
