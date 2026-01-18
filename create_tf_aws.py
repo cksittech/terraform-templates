f8a671a35bb4ae8ed5ea886a04420727bca8e7d9
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-01-18 14:24:15 +0900

    update create_tf_aws.py

--- create_tf_aws.py
+++ create_tf_aws.py
@@ -1,45 +1,53 @@
 import json
 import os
 
-path = "./schema.json"
-with open(path, "r", encoding="utf-8") as f:
-    data = json.load(f)
-type_list = []
-resource_schemas = data["provider_schemas"]["registry.terraform.io/hashicorp/aws"]["resource_schemas"]
+TYPE_DEFAULTS = {
+    "number": "0",
+    "string": "\"\"",
+    "bool": "false",
+    "list": "[]",
+    "set": "[]",
+    "map": "{}",
+}
 
-for resource in resource_schemas.keys():
-    path = f"./output/{resource.split('_')[1]}"
-    os.makedirs(path, exist_ok=True)
-    with open(f"./{path}/{resource[4:]}.tf", "w", encoding="utf-8") as f:
-        f.write(f"resource \"{resource}\" \"tf-sample-{resource[4:].replace('_','-')}\" {{\n")
-        tags_exist = True if "tags" in list(resource_schemas[resource]["block"]["attributes"].keys()) else False
-        attributes = [attribute for attribute in list(resource_schemas[resource]["block"]["attributes"].keys()) 
+CLOUD_PROVIDER_NAME = "aws"
+
+SCHEMA_PATH = f".\\schema.json"
+
+with open(SCHEMA_PATH, "r", encoding="utf-16") as f:
+    RESOURCE_SCHEMAS = json.loads(f.read().replace('窶・,"description_kind"', '窶・","description_kind"'))["provider_schemas"][f"registry.terraform.io/hashicorp/{CLOUD_PROVIDER_NAME}"]["resource_schemas"]
+type_list = []
+
+for resource in RESOURCE_SCHEMAS.keys():
+    OUTPUT_PATH = f".\\output\\{resource.split('_')[1]}"
+    os.makedirs(OUTPUT_PATH, exist_ok=True)
+    with open(f".\\{OUTPUT_PATH}\\{"_".join(resource.split('_')[1:])}.tf", "w", encoding="utf-8") as f:
+        f.write(f"resource \"{resource}\" \"tf-sample-{"-".join(resource.split('_')[1:])}\" {{\n")
+        tags_exist = True if "tags" in list(RESOURCE_SCHEMAS[resource]["block"]["attributes"].keys()) else False
+        attributes = [attribute for attribute in list(RESOURCE_SCHEMAS[resource]["block"]["attributes"].keys()) 
                       if not(attribute in ["arn", "tags_all", "tags", "id"]) 
-                      and ("optional" in resource_schemas[resource]["block"]["attributes"][attribute].keys() 
-                           or "required" in resource_schemas[resource]["block"]["attributes"][attribute].keys())]
-        if "block_types" in list(resource_schemas[resource]["block"].keys()):
-            block_types = [block_type for block_type in list(resource_schemas[resource]["block"]["block_types"].keys()) if not(block_type in ["timeouts"])]
+                      and ("optional" in RESOURCE_SCHEMAS[resource]["block"]["attributes"][attribute].keys() 
+                           or "required" in RESOURCE_SCHEMAS[resource]["block"]["attributes"][attribute].keys())]
+        if "block_types" in list(RESOURCE_SCHEMAS[resource]["block"].keys()):
+            block_types = [block_type for block_type in list(RESOURCE_SCHEMAS[resource]["block"]["block_types"].keys()) if not(block_type in ["timeouts"])]
         else:
             block_types = []
+        
         if attributes:
-            fill_nun = max(len(s) for s in attributes)
+            fill_num = max(len(s) for s in attributes)
             attributes.sort()
             for attribute in attributes:
-                if isinstance(resource_schemas[resource]["block"]["attributes"][attribute]["type"], list):
-                    if resource_schemas[resource]["block"]["attributes"][attribute]["type"][0] == "object":
+                attr_type = RESOURCE_SCHEMAS[resource]["block"]["attributes"][attribute]["type"]
+                if isinstance(attr_type, list):
+                    base_type = attr_type[0]
+                    if base_type == "object":
                         block_types.append(attribute)
-                    elif resource_schemas[resource]["block"]["attributes"][attribute]["type"][0] == "set":
-                        f.write(f"  {attribute.ljust(fill_nun, ' ')} = []\n")
-                    elif resource_schemas[resource]["block"]["attributes"][attribute]["type"][0] == "list":
-                        f.write(f"  {attribute.ljust(fill_nun, ' ')} = []\n")
-                    elif resource_schemas[resource]["block"]["attributes"][attribute]["type"][0] == "map":
-                        f.write(f"  {attribute.ljust(fill_nun, ' ')} = {{}}\n")
-                elif resource_schemas[resource]["block"]["attributes"][attribute]["type"] == "number":
-                    f.write(f"  {attribute.ljust(fill_nun, ' ')} = 0\n")
-                elif resource_schemas[resource]["block"]["attributes"][attribute]["type"] == "string":
-                    f.write(f"  {attribute.ljust(fill_nun, ' ')} = \"\"\n")
-                elif resource_schemas[resource]["block"]["attributes"][attribute]["type"] == "bool":
-                    f.write(f"  {attribute.ljust(fill_nun, ' ')} = false\n")
+                        continue
+                else:
+                    base_type = attr_type
+                default = TYPE_DEFAULTS.get(base_type)
+                if default is not None:
+                    f.write(f"  {attribute.ljust(fill_num)} = {default}\n")
         
         block_types.sort()
         if len(block_types):
