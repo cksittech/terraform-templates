4208db6147dcef0910b2159e26e25e3ba75310d4
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-02-15 01:46:51 +0000

    create or update at v1.82.68

---
+++ Tencent/tf-sample/v1/waf/tencentcloud_waf_bot_id_rule.tf
@@ -0,0 +1,14 @@
+resource "tencentcloud_waf_bot_id_rule" "tf-sample-waf-bot-id-rule" {
+  domain        = ""
+  global_switch = 0
+  protect_level = ""
+  scene_id      = ""
+  
+  data {
+    action   = ""
+    bot_id   = ""
+    redirect = ""
+    rule_id  = ""
+    status   = false
+  }
+}
