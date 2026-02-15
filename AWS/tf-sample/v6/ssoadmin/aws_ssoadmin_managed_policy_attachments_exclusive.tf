b32f7ff2d31b5b07e9b6be78986f4e97c91e58a1
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-02-15 01:06:56 +0000

    create or update at v6.30.0

---
+++ AWS/tf-sample/v6/ssoadmin/aws_ssoadmin_managed_policy_attachments_exclusive.tf
@@ -0,0 +1,6 @@
+resource "aws_ssoadmin_managed_policy_attachments_exclusive" "tf-sample-ssoadmin-managed-policy-attachments-exclusive" {
+  instance_arn        = ""
+  managed_policy_arns = []
+  permission_set_arn  = ""
+  region              = ""
+}
