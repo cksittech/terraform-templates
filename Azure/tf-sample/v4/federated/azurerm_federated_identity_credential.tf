cf01f794f3a1eab39652a91417dd517e89ac6a79
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-02-15 01:12:42 +0000

    create or update at v4.60.0

--- Azure/tf-sample/v4/federated/azurerm_federated_identity_credential.tf
+++ Azure/tf-sample/v4/federated/azurerm_federated_identity_credential.tf
@@ -1,8 +1,7 @@
 resource "azurerm_federated_identity_credential" "tf-sample-federated-identity-credential" {
-  audience            = []
-  issuer              = ""
-  name                = ""
-  parent_id           = ""
-  resource_group_name = ""
-  subject             = ""
+  audience  = []
+  issuer    = ""
+  name      = ""
+  parent_id = ""
+  subject   = ""
 }
--- Azure/tf-sample/v4/mssql/azurerm_mssql_server_transparent_data_encryption.tf
+++ Azure/tf-sample/v4/mssql/azurerm_mssql_server_transparent_data_encryption.tf
@@ -1,6 +1,5 @@
 resource "azurerm_mssql_server_transparent_data_encryption" "tf-sample-mssql-server-transparent-data-encryption" {
   auto_rotation_enabled = false
   key_vault_key_id      = ""
-  managed_hsm_key_id    = ""
   server_id             = ""
 }
