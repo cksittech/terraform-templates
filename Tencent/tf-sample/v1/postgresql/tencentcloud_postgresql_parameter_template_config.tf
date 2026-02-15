a75052ddc9f8cde5d5cfe5c8573459bf01f28477
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-02-15 01:48:14 +0000

    create or update at v1.82.70

---
+++ Tencent/tf-sample/v1/postgresql/tencentcloud_postgresql_parameter_template_config.tf
@@ -0,0 +1,8 @@
+resource "tencentcloud_postgresql_parameter_template_config" "tf-sample-postgresql-parameter-template-config" {
+  template_id = ""
+  
+  modify_param_entry_set {
+    expected_value = ""
+    name           = ""
+  }
+}
