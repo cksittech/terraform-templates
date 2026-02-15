0e211dbedf7c14003c8d8478b5273652c3675e2f
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-02-15 10:31:30 +0900

    update tencentcloud_version_pinned.tf

--- providers/tencentcloud_version_pinned.tf
+++ providers/tencentcloud_version_pinned.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     tencentcloud = {
-      source = "tencentcloudstack/tencentcloud"
+      source  = "tencentcloudstack/tencentcloud"
       version = "TERRAFORM_PROVIDER_VERSION_PINNED"
     }
   }
