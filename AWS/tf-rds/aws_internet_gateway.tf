f536894c83d5b5f417cfead35635b3e2aca70a87
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-03-01 18:42:23 +0900

    delete tf-rds

--- AWS/tf-rds/aws_internet_gateway.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_internet_gateway" "tf-rdscluster-internet-gateway" {
-  # region = ""
-  vpc_id = aws_vpc.tf-rdscluster-vpc.id
-
-  tags = {
-    Name = "${var.terraform}-internet-gateway"
-    Terraform = var.terraform
-  }
-}
--- AWS/tf-rds/aws_main_route_table_association.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_main_route_table_association" "tf-rdscluster-main-route-table-association" {
-  # region         = ""
-  route_table_id = aws_route_table.tf-rdscluster-route-table-main.id
-  vpc_id         = aws_vpc.tf-rdscluster-vpc.id
-}
