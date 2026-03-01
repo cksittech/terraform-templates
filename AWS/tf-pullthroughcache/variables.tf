0740a7072b50dc311d32d4cad0a5bbe8eeb19a61
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-03-01 18:11:38 +0900

    update tf-pullthroughcache

--- AWS/tf-pullthroughcache/variables.tf
+++ AWS/tf-pullthroughcache/variables.tf
@@ -9,17 +9,6 @@ variable "GITHUB_TOKEN" {}
 
 
 
-variable "availability_zone" {
-  default = {
-    ap_northeast_1 = {
-      ap_northeast_1a = "ap-northeast-1a"
-      ap_northeast_1c = "ap-northeast-1c"
-      ap_northeast_1d = "ap-northeast-1d"
-    }
-  }
-  description = "Availability zone name."
-}
-
 variable "terraform" {
   default     = "tf-pullthroughcache"
 }
