e2e9a0e41196c461b98f98ae93f0d38bc7ee0417
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-03-29 15:10:53 +0000

    create or update at v4.66.0

--- Azure/tf-sample/v4/cognitive/azurerm_cognitive_account_rai_blocklist.tf
+++ Azure/tf-sample/v4/cognitive/azurerm_cognitive_account_rai_blocklist.tf
@@ -2,4 +2,6 @@ resource "azurerm_cognitive_account_rai_blocklist" "tf-sample-cognitive-account-
   cognitive_account_id = ""
   description          = ""
   name                 = ""
+  
+  tags = {}
 }
