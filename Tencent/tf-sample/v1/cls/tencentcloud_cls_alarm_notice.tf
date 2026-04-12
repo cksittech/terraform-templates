ddc3ac558939e58683656584cc1ad0b91a2380fb
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-04-12 16:03:17 +0000

    create or update at v1.82.85

--- Tencent/tf-sample/v1/cls/tencentcloud_cls_alarm_notice.tf
+++ Tencent/tf-sample/v1/cls/tencentcloud_cls_alarm_notice.tf
@@ -1,7 +1,16 @@
 resource "tencentcloud_cls_alarm_notice" "tf-sample-cls-alarm-notice" {
-  name = ""
-  type = ""
+  alarm_shield_status = 0
+  callback_prioritize = false
+  deliver_status      = 0
+  jump_domain         = ""
+  name                = ""
+  type                = ""
   
+  deliver_config {
+    region   = ""
+    scope    = 0
+    topic_id = ""
+  }
   notice_receivers {
     end_time          = ""
     index             = 0
@@ -10,6 +19,59 @@ resource "tencentcloud_cls_alarm_notice" "tf-sample-cls-alarm-notice" {
     receiver_ids      = []
     receiver_type     = ""
     start_time        = ""
+  }
+  notice_rules {
+    escalate = false
+    interval = 0
+    rule     = ""
+    type     = 0
+    
+    escalate_notices {
+      escalate = false
+      interval = 0
+      type     = 0
+      
+      notice_receivers {
+        end_time          = ""
+        index             = 0
+        notice_content_id = ""
+        receiver_channels = []
+        receiver_ids      = []
+        receiver_type     = ""
+        start_time        = ""
+      }
+      web_callbacks {
+        callback_type     = ""
+        index             = 0
+        method            = ""
+        mobiles           = []
+        notice_content_id = ""
+        remind_type       = 0
+        url               = ""
+        user_ids          = []
+        web_callback_id   = ""
+      }
+    }
+    notice_receivers {
+      end_time          = ""
+      index             = 0
+      notice_content_id = ""
+      receiver_channels = []
+      receiver_ids      = []
+      receiver_type     = ""
+      start_time        = ""
+    }
+    web_callbacks {
+      callback_type     = ""
+      index             = 0
+      method            = ""
+      mobiles           = []
+      notice_content_id = ""
+      remind_type       = 0
+      url               = ""
+      user_ids          = []
+      web_callback_id   = ""
+    }
   }
   web_callbacks {
     callback_type     = ""
