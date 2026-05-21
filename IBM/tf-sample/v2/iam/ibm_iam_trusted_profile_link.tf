1468baa650d6f63762ca098726cb6907083ec43a
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-05-21 15:41:06 +0000

    create or update at v2.2.1

--- IBM/tf-sample/v2/iam/ibm_iam_trusted_profile_link.tf
+++ IBM/tf-sample/v2/iam/ibm_iam_trusted_profile_link.tf
@@ -1,7 +1,8 @@
 resource "ibm_iam_trusted_profile_link" "tf-sample-iam-trusted-profile-link" {
-  cr_type    = ""
-  name       = ""
-  profile_id = ""
+  cr_type          = ""
+  is_cross_account = false
+  name             = ""
+  profile_id       = ""
   
   link {
     component_name = ""
---
+++ IBM/tf-sample/v2/logs/ibm_logs_log_data_retention_tags.tf
@@ -0,0 +1,7 @@
+resource "ibm_logs_log_data_retention_tags" "tf-sample-logs-log-data-retention-tags" {
+  endpoint_type = ""
+  instance_id   = ""
+  region        = ""
+  
+  tags = {}
+}
