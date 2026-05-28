744618ebae33114d55d4f235de734345778c321a
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-05-28 15:41:04 +0000

    create or update at v2.2.2

---
+++ IBM/tf-sample/v2/cis/ibm_cis_dns_records_batch.tf
@@ -0,0 +1,38 @@
+resource "ibm_cis_dns_records_batch" "tf-sample-cis-dns-records-batch" {
+  cis_id    = ""
+  domain_id = ""
+  
+  deletes {
+    id = ""
+  }
+  patches {
+    content  = ""
+    data     = {}
+    id       = ""
+    name     = ""
+    priority = 0
+    proxied  = false
+    ttl      = 0
+    type     = ""
+  }
+  posts {
+    content  = ""
+    data     = {}
+    id       = ""
+    name     = ""
+    priority = 0
+    proxied  = false
+    ttl      = 0
+    type     = ""
+  }
+  puts {
+    content  = ""
+    data     = {}
+    id       = ""
+    name     = ""
+    priority = 0
+    proxied  = false
+    ttl      = 0
+    type     = ""
+  }
+}
