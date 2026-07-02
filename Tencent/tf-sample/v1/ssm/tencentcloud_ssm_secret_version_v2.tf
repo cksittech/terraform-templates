826132f516c65622a94f5eddf7fb17d218f29d2e
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-07-02 16:01:36 +0000

    create or update at v1.83.7

---
+++ Tencent/tf-sample/v1/ssm/tencentcloud_ssm_secret_version_v2.tf
@@ -0,0 +1,7 @@
+resource "tencentcloud_ssm_secret_version_v2" "tf-sample-ssm-secret-version-v2" {
+  secret_binary    = ""
+  secret_name      = ""
+  secret_string    = ""
+  secret_string_wo = ""
+  version_id       = ""
+}
