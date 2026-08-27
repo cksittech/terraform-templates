cd58d90898efc9c1df4c6d8f0ea8116b588d0400
cksittech <zerg.cfa44.cksittech@gmail.com>
2026-08-27 15:50:52 +0000

    create or update at v1.290.0

---
+++ Alicloud/tf-sample/v1/mongodb/alicloud_mongodb_sharding_audit_filter.tf
@@ -0,0 +1,9 @@
+resource "alicloud_mongodb_sharding_audit_filter" "tf-sample-mongodb-sharding-audit-filter" {
+  audit_status       = ""
+  db_instance_id     = ""
+  filter             = ""
+  hot_storage_period = 0
+  role_type          = ""
+  service_type       = ""
+  storage_period     = 0
+}
---
+++ Alicloud/tf-sample/v1/polardb/alicloud_polardb_gateway.tf
@@ -0,0 +1,12 @@
+resource "alicloud_polardb_gateway" "tf-sample-polardb-gateway" {
+  auto_renew        = false
+  db_cluster_class  = ""
+  db_type           = ""
+  pay_type          = ""
+  period            = ""
+  security_group_id = ""
+  used_time         = 0
+  vpc_id            = ""
+  vswitch_id        = ""
+  zone_id           = ""
+}
